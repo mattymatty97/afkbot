546b7ae287bf1fafb9053c35403bad7890e4d50d
mattymatty <matteo.francesconi@davide.it>
2021-01-21 21:12:29 +0100

    testing fixes for https

--- public/script.js
+++ public/script.js
@@ -6,9 +6,9 @@ function login() {
     xhr.onreadystatechange = function () {
         if (this.readyState === 4){
             if (this.status === 200) {
-                document.cookie = "user=" + document.getElementsByClassName('userid')[0].value +";samesite=strict"
-                document.cookie = "password=" + document.getElementsByClassName('password')[0].value +";samesite=strict"
-                window.location.href = window.location.href + "instance";
+                document.cookie = "user=" + document.getElementsByClassName('userid')[0].value +";samesite=strict"+(location.protocol==="https:")?";Secure=true":""
+                document.cookie = "password=" + document.getElementsByClassName('password')[0].value +";samesite=strict"+(location.protocol==="https:")?";Secure=true":""
+                window.location.href = window.location.origin + "instance";
             }else{
             document.getElementById("error").innerHTML = "<a>Error:<br><br>Wrong UserId or Password</a>"
             }
